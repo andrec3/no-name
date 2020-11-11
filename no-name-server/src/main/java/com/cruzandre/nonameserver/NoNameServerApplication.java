8bdbd85b54568948e489304359812555b66a1545
Andre Cruz <andrec3@gmail.com>
2020-11-11 14:14:17 -0500

    Initial commit

---
+++ no-name-server/src/main/java/com/cruzandre/nonameserver/NoNameServerApplication.java
@@ -0,0 +1,13 @@
+package com.cruzandre.nonameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoNameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoNameServerApplication.class, args);
+	}
+
+}
---
+++ no-name-server/src/test/java/com/cruzandre/nonameserver/NoNameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruzandre.nonameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoNameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
